839885fc1abbf67a8655332cd9e3b64bea865c65
Dan Brook <dan@broquaint.com>
2023-02-19 21:40:09 +0000

    Add sprite rotation to utils
    Have wanted this for a while now, still not sure how to use it good,
    but got something going.

--- utils.lua
+++ utils.lua
@@ -176,3 +176,19 @@ function nice_pos(num)
    end
    return (s == -1 and '-' or '') .. sig .. '.' .. frc
 end
+
+-- via https://www.lexaloffle.com/bbs/?pid=78451
+function spr_rotate(x,y,rot,mx,my,w,flip,scale)
+  scale=scale or 1
+  w*=scale*4
+
+  local cs, ss = cos(rot)*.125/scale,sin(rot)*.125/scale
+  local sx, sy = mx+cs*-w, my+ss*-w
+  local hx = flip and -w or w
+
+  local halfw = -w
+  for py=y-w, y+w do
+    tline(x-hx, py, x+hx, py, sx-ss*halfw, sy+cs*halfw, cs, ss)
+    halfw+=1
+  end
+end
